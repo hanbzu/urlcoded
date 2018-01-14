489924616da7bf0b1f8edbae64599ad7f5512105
Hibai Unzueta <hibai.unzueta@gmail.com>
2018-01-15 00:16:49 +0100

    Issue code length warning on zip task

--- scripts/zip.js
+++ scripts/zip.js
@@ -4,6 +4,9 @@ const atob = require('atob')
 const btoa = require('btoa')
 const ncp = require('copy-paste')
 
+const MAX_URL_LENGTH = 2000
+const GUESSED_ROOT_URL_LENGTH = 20
+
 const path = process.argv[2]
 if (path === undefined) {
   throw new Error('You need to specify the location of the markdown file to be zipped!')
@@ -13,4 +16,7 @@ const markdown = fs.readFileSync(path, 'utf8')
 const compressedAndBase64encoded = btoa(pako.gzip(markdown, { to: 'string' }))
 ncp.copy(compressedAndBase64encoded)
 
-console.log('Code copied to clipboard!')
+console.log(`${compressedAndBase64encoded.length} chars. Code copied to clipboard!`)
+if (compressedAndBase64encoded.length > MAX_URL_LENGTH - GUESSED_ROOT_URL_LENGTH) {
+  console.error('This code is so large that will very likely produce an invalid URL!')
+}
